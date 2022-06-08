fb4dcb2bbd6b0e576a0417a5006c211596156e70
AymarBly5591 <100754609+AymarBly5591@users.noreply.github.com>
2022-06-08 19:22:58 +0000

    back office

--- src/components/AdminProduit/ListProducts.js
+++ src/components/AdminProduit/ListProducts.js
@@ -48,19 +48,14 @@ const ListProducts = ({products}) => {
        <div class="mb-1 w-full">
           <div class="mb-4">
              <nav class="flex mb-5" aria-label="Breadcrumb">
-                <ol class="inline-flex items-center space-x-1 md:space-x-2">
+                <ol class="inline-flex items-center border-b space-x-1 md:space-x-2">
                    <li class="inline-flex items-center">
                       <a href="#" class="text-gray-700 hover:text-gray-900 inline-flex items-center">
                          <svg class="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
                          Home
                        </a>
                    </li>
-                    <li>
-                        <div class="flex items-center">
-                            <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                            <a href="#" class="text-gray-700 hover:text-gray-900 ml-1 md:ml-2 text-sm font-medium">E-commerce</a>
-                       </div>
-                   </li>
+                    
                    <li>
                         <div class="flex items-center">
                             <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
@@ -69,7 +64,7 @@ const ListProducts = ({products}) => {
                    </li>
                </ol>
            </nav>
-           <h1 class="text-xl sm:text-2xl font-semibold text-gray-900">Tous les produits</h1>
+           <h1 class="text-xl sm:text-2xl font-semibold text-gray-900 border-b">Tous les produits</h1>
            </div>
            <div class="block sm:flex items-center md:divide-x md:divide-gray-100">
                    <form class="sm:pr-3 mb-4 sm:mb-0 flex   " action="#" method="GET">
